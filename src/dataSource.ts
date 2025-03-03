f1a05c88907631ada9a90c841eabe684e04ea164
asadd2723 <asadu3040@gmail.com>
2025-03-03 12:54:20 +0500

    push code

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -9,10 +9,6 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL, // Use Heroku DATABASE_URL
   synchronize: true, // Change to false in production (use migrations instead)
-  logging: false,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : undefined, // Required for Heroku
+  logging: true,
   entities: [User], // Update path as needed
 });
